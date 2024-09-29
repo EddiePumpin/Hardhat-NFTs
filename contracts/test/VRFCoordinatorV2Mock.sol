a122321f2e3c86365fe2c7340422955ba80b167d
EddiePumpin <eddiepumpin759@gmail.com>
2024-09-30 00:21:37 +0100

    new changes

--- contracts/test/VRFCoordinatorV2Mock.sol
+++ contracts/test/VRFCoordinatorV2Mock.sol
@@ -1,5 +1,3 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.20;
-
-import "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+pragma solidity ^0.8.7;
+import "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2Mock.sol";
